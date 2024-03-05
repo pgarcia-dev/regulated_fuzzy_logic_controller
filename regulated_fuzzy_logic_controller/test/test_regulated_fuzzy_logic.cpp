40463a82bbcaa64cd05dcc64ba3a317876e91346
pgarcia-dev <106911922+pgarcia-dev@users.noreply.github.com>
2024-03-05 13:43:04 +0100

    added main to test

--- regulated_fuzzy_logic_controller/test/test_regulated_fuzzy_logic.cpp
+++ regulated_fuzzy_logic_controller/test/test_regulated_fuzzy_logic.cpp
@@ -41,3 +41,11 @@ TEST(RegulatedPurePursuitTest, createCarrotMsg)
 {
   EXPECT_EQ(0, 0);
 }
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
+
+  return RUN_ALL_TESTS();
+}
